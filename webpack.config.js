68ca3fa2576d9be6d7a0051cd4da61c37e2cb454
Waihon Yew <yewwaihon@gmail.com>
2021-03-21 12:27:48 +0000

    Process every .js using babel-loader excluding node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   entry: './index.js',
   output: {
-    path: './dist',
+    path: __dirname + '/dist',
+    publicPath: '/dist/',
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }]
   }
-}
+};
